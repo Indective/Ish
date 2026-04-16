8868114d2e0e7101470f60133a987da28349a069
indective <ibrahemmohamd561@gmail.com>
2026-04-16 15:29:46 +0300

    Refactor + minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "Parsing.hpp"
 #include "Executing.hpp"
-#include "Environment.hpp"
+#include "ShellContext.hpp"
 #include "JobControl.hpp"
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -11,43 +11,34 @@
 
 int main()
 {
-    Environment environ;
-    ExecResult result = ExecResult::OK;
+    ShellContext shell;
+    ExecResult result = ExecResult::Continue;
 
-    const size_t MAX_BUFFER_LENGTH = 1024;
-    char buffer[MAX_BUFFER_LENGTH]; // save the path
-    char* full_path;
-    char* input;
-
-    environ.load_aliases();
+    shell.load_aliases();
     signal(SIGCHLD, JobControl::sigchldHandler);
 
-    while(result != ExecResult::EXIT)
-    {
-        
-        full_path = getcwd(buffer,MAX_BUFFER_LENGTH); 
+    while(result != ExecResult::Exit)
+    {   
+        const size_t MAX_BUFFER_LENGTH = 1024;
+        char buffer[MAX_BUFFER_LENGTH]; // save the path
+        const char* full_path = getcwd(buffer,MAX_BUFFER_LENGTH); 
 
-        input = environ.get_input(environ.build_prompt(full_path).c_str());
+        const char * input = shell.get_input(shell.build_prompt(full_path).c_str());
 
         if(!input) // avoid dangling pointers that the program tries to parse later on (causing core dumps)
         {
             break;
         }
-
+        
         std::optional<PipeLine> p = CommandParsing::parse_command(input);  
         
-        if(pipe)
+        if(p)
         {
-            for(auto &it : p->commands)
-            {
-                environ.replace_alias(it); // exppand aliases in all commands 
-            }
             result = CommandExecuting::handle_execution(p);
         }
 
         JobControl::reap_finished_jobs();
         
-        free(input); // (hopefully) avoid segfaults
     }
 
     return 0;
